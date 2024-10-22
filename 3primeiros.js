2133d3dddaf94949370a17e774a5e139cb67fda3
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-10-22 20:57:20 -0300

    ok

---
+++ 3primeiros.js
@@ -0,0 +1,7 @@
+fetch('https://pokeapi.co/api/v2/type')
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < 3; i++) {
+            console.log('Tipo:', data.results[i].name);
+        }
+    });
---
+++ idAleatorio.js
@@ -0,0 +1,4 @@
+let id = Math.floor(Math.random() * 100) + 1;
+fetch('https://pokeapi.co/api/v2/pokemon/${id}')
+    .then(response => response.json())
+    .then(data)
