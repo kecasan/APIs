c68ce90fcb0c89966db25815948a0b920ea0b1b1
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-10-24 11:20:16 -0300

    ok

--- idAleatorio.js
+++ idAleatorio.js
@@ -1,5 +1,5 @@
 let id = Math.floor(Math.random() * 100) + 1;
-fetch('https://pokeapi.co/api/v2/pokemon/${id}')
+fetch('https://pokeapi.co/api/v2/pokemon/' + id)
     .then(response => response.json())
     .then(data => {
         console.log('Pokémon:', data.name);
