b330fa1892dee99415305e257700c875fa09f7df
Érica Santos <143044036+kecasan@users.noreply.github.com>
2024-10-23 21:25:43 -0300

    ok

--- starWars.js
+++ starWars.js
@@ -1,7 +1,6 @@
 function buscaPersonagem() {
     var personagem = document.getElementById("personagem").value;
-    var url = "https://swapi.dev/api/people/?search=${personagem}";
-
+    var url = "https://swapi.dev/api/people/?search=" + personagem;
     fetch(url)
         .then(function(response) {
             return response.json();
@@ -12,5 +11,8 @@ function buscaPersonagem() {
             for (var i = 0; i < data.name ; i++) {
                 descricao += '<li>' + data.name[i].name + '</li>';
             }
-        })
+
+            document.getElementById('resultado').innerHTML = 
+                '<p><strong>Nome:</strong> ' + data.name + '</p>';
+        });
 }
